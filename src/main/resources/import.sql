70e482bcea8b21b9e4d0f31c5f871efa030cfd36
Andre Aranda Salazar <214158892+AndreArandaSalazar@users.noreply.github.com>
2026-06-30 23:44:51 -0500

    config: agregar docker compose, propiedades de BD y scrip semilla

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, correo, password, rol, estado) 
+VALUES ('Admin', 'Sistema', '1990-01-01', 'admin@organizacion.com', 'admin123', 'ADMINISTRADOR', 'ACTIVO');
+
+INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, correo, password, rol, estado) 
+VALUES ('Andre', 'Aranda Salazar', '13-02-1997', 'n00322350@upn.pe', NULL, 'CLIENTE', 'PENDIENTE');
+
+INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, correo, password, rol, estado) 
+VALUES ('Harol', 'Aranda Salazar', '13-02-1997', 'n00322364@upn.pe', NULL, 'CLIENTE', 'PENDIENTE');
